13d59f49bb20526ebf8b9772ba54d6dd9027ffad
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-09-18 14:09:32 +0200

    home route configured

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import homeRouter from "./routes/homeRouter.js";
 
 // .env file is at root
 dotenv.config();
@@ -10,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}));
 
-//app.use("/", homeRouter);
+app.use("/", homeRouter);
 
 app.listen(PORT, () => {
     console.log(`Express app is running on port:${PORT}`);
