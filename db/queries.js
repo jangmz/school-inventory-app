ce64fa17480be8d08942f3d183b9303157ca44fa
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-09-20 10:36:24 +0200

    added some basic queries

--- db/queries.js
+++ db/queries.js
@@ -1,6 +1,17 @@
 import pool from "./pool.js";
 
 // get all laptops from DB
-
+async function getAllLaptops() {
+    const { rows } = await pool.query("SELECT * FROM laptops;");
+    return rows;
+}
 
 // get all tablets from DB
+async function getAllTablets() {
+    const {rows} = await pool.query("SELECT * FROM tablets;");
+    return rows;
+}
+
+export default {
+    getAllLaptops,
+}
