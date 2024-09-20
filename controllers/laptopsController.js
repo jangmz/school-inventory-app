b785b908754f08c73250f1ab6d6cae6e455f741d
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-09-20 10:50:10 +0200

    calling query to display laptops on the webapp

--- controllers/laptopsController.js
+++ controllers/laptopsController.js
@@ -1,9 +1,13 @@
+import db from "../db/queries.js";
+
 let laptopsController = {};
 
 // GET /laptops -> displays all laptops in the DB
-function laptopsGet(req, res) {
-
-    res.render("laptops");
+async function laptopsGet(req, res) {
+    const laptopsList = await db.getAllLaptops();
+    res.render("laptops", {
+        laptopsList
+    });
 }
 
 export default laptopsController = {
