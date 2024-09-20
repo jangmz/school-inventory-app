29ff73cfdbf0b5e2ce59c188c751ca8f24f27b17
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-09-20 10:01:33 +0200

    added error handling and fixed sql queries

--- db/populateDB.js
+++ db/populateDB.js
@@ -8,14 +8,14 @@ dotenv.config();
 
 const sqlTables = `
     CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+        id SERIAL PRIMARY KEY NOT NULL,
         first_name VARCHAR(255) NOT NULL,
         last_name VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL
+        email VARCHAR(255) UNIQUE NOT NULL
     );
 
     CREATE TABLE IF NOT EXISTS laptops (
-        id INTEGER PRIMARY KEY NOT NULL,
+        id SERIAL PRIMARY KEY NOT NULL,
         model VARCHAR(255),
         status VARCHAR(255) NOT NULL,
         user_id INTEGER REFERENCES users(id) ON DELETE cascade,
@@ -23,31 +23,35 @@ const sqlTables = `
         cpu VARCHAR(255),
         ram INTEGER,
         storage INTEGER,
-        notes VARCHAR(255)
+        notes TEXT
     );
 
     CREATE TABLE IF NOT EXISTS tablets (
-        id INTEGER PRIMARY KEY NOT NULL,
+        id SERIAL PRIMARY KEY NOT NULL,
         model VARCHAR(255),
         status VARCHAR(255) NOT NULL,
         user_id INTEGER REFERENCES users(id) ON DELETE cascade,
-        notes VARCHAR(255),
-        doc_signed BOOLEAN NOT NULL
+        notes TEXT,
+        doc_signed BOOLEAN NOT NULL DEFAULT FALSE
     );
 `;
 
 async function main() {
-    console.log("Creating...");
+    try {
+        console.log("Creating...");
 
-    const client = new Client({
-        connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost:${process.env.DB_PORT}/${process.env.DB_NAME}`
-    });
+        const client = new Client({
+            connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost:${process.env.DB_PORT}/${process.env.DB_NAME}`
+        });
 
-    await client.connect();
-    await client.query(sqlTables);
-    await client.end();
+        await client.connect();
+        await client.query(sqlTables);
+        await client.end();
 
-    console.log("Finished.");
+        console.log("Finished.");
+    } catch (error) {
+        console.log(error);   
+    }
 }
 
 main();
