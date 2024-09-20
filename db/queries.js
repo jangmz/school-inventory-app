12d95202dab7ef8e574f34d9772af7804f20bf05
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-09-20 11:28:31 +0200

    sql for inserting a new laptop

--- db/queries.js
+++ db/queries.js
@@ -12,6 +12,14 @@ async function getAllTablets() {
     return rows;
 }
 
+// insert new laptop in to DB
+async function insertLaptop(laptop) {
+    await pool.query(`
+        INSERT INTO laptops(id, model, status, user_id, doc_signed, cpu, ram, storage, notes)
+        VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9);   
+    `, [laptop.id, laptop.model, laptop.status, laptop.user_id, laptop.doc_signed, laptop.cpu, laptop.ram, laptop.storage, laptop.notes]);
+}
+
 export default {
     getAllLaptops,
     getAllTablets,
