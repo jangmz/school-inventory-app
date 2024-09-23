74341099eb2a81b0b347c1459206f47c5322bfb6
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-09-23 15:05:43 +0200

    added updating of laptop data

--- controllers/laptopsController.js
+++ controllers/laptopsController.js
@@ -2,6 +2,17 @@ import db from "../db/queries.js";
 
 let laptopsController = {};
 
+// function that transforms data into correct formats
+function laptopDataCorrection(laptop) {
+    laptop.id = laptop.id ? parseInt(laptop.id) : null;
+    laptop.user_id = laptop.user_id ? parseInt(laptop.user_id) : null;
+    laptop.ram = laptop.ram ? parseInt(laptop.ram) : null;
+    laptop.storage = laptop.storage ? parseInt(laptop.storage) : null;
+    laptop.doc_signed = laptop.doc_signed === "on" ? true : false;
+
+    return laptop;
+}
+
 // GET /laptops -> displays all laptops in the DB
 async function laptopsGet(req, res) {
     const laptopsList = await db.getAllLaptops();
@@ -50,10 +61,29 @@ async function laptopsUpdateGet(req, res) {
 }
 
 // POST /laptops/update/:laptopId -> updates the data to the DB and redirects to /laptops
+async function laptopsUpdatePost(req, res) {
+    try {
+        console.log("Updating database - laptop table...");
+        let updatedLaptopData = req.body;
+        updatedLaptopData.id = req.params.laptopId;
+
+        // correct data types
+        updatedLaptopData = laptopDataCorrection(updatedLaptopData);
+
+        // update in DB
+        await db.updateLaptop(updatedLaptopData);
+    } catch (error) {
+        console.log(error);
+    }
+
+    console.log("Update successfull.");
+    res.redirect("/laptops");
+}
 
 export default laptopsController = {
     laptopsGet,
     laptopsNewGet,
     laptopsNewPost,
     laptopsUpdateGet,
+    laptopsUpdatePost,
 }
--- routes/laptopsRouter.js
+++ routes/laptopsRouter.js
@@ -7,5 +7,6 @@ laptopsRouter.get("/", laptopsController.laptopsGet);
 laptopsRouter.get("/new", laptopsController.laptopsNewGet);
 laptopsRouter.post("/new", laptopsController.laptopsNewPost);
 laptopsRouter.get("/update/:laptopId", laptopsController.laptopsUpdateGet);
+laptopsRouter.post("/update/:laptopId", laptopsController.laptopsUpdatePost);
 
 export default laptopsRouter;
