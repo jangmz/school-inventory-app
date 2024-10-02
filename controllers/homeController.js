cf8a40ee2d68c1d4350bf8226f836b35387d2042
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-10-02 11:57:19 +0200

    removed some logs

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,14 +2,12 @@ import db from "../db/queries.js";
 
 // GET / -> home page displaying current stock statistics
 async function statisticsLaptopsGet(req, res) {
-    console.log("Gathering data...");
     const totalLaptops = await db.totalLaptops();
     const availableLaptops = await db.availableLaptops();
     const unavailableLaptops = await db.unavailableLaptops();
     const unknownLocationLaptops = await db.unknownLocationLaptops();
     const reservedLaptops = await db.reservedLaptops();
     const notInUseLaptops = await db.notInUseLaptops();
-    console.log("Finished.");
 
     res.render("index", {
         totalLaptops,
